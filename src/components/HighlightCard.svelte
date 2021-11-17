200639c975dc0f47d1f6356ce9fdf708261738f6
Jim Medlock <jdmedlock@gmail.com>
2021-11-16 20:38:17 -0600

    Feature: Refine text placement on card

--- src/components/HighlightCard.svelte
+++ src/components/HighlightCard.svelte
@@ -1,6 +1,22 @@
-<div class="relative inset-0 z-10 m-8">
-  <div class="flex transform rotate-2 rounded-lg drop-shadow-2xl w-1/4 h-1/2 m-12">
-    <div class="absolute z-0 text-black text-2xl text-center">Hi Xync</div>
-    <img class="-m-12" src="/src/assets/green-card.png" alt="green card">
+<div class="flex-none relative inset-0 mt-16 w-2/3 h-3/4 place-self-center">
+  <div class="transform rotate-2 drop-shadow-2xl ">
+    <!-- Background card image -->
+    <img class="absolute rounded-xl z-0" src="/src/assets/green-card.png" alt="green card">
+
+    <!-- Content overlaying the card -->
+    <div class="flex-none grid-rows-3 gap-1">
+      <!-- Row #1-->
+      <div class="flex w-full row-start-1 grid-cols-3 justify-evenly">
+        <div class="col-start-1 text-black text-2xl m-4 z-50">Hi Xync</div>
+        <div class="col-start-2 text-black text-2xl m-4 z-50">Hi Xync</div>
+        <div class="col-start-3 text-black text-2xl m-4 z-50">Hi Xync</div>
+      </div>
+      <!-- Row #2 -->
+      <div class="flex w-full row-start-2 grid-cols-3 justify-evenly">
+        <div class="col-start-1 text-black text-2xl m-4 z-50">Hi Xync</div>
+        <img class="col-start-2 w-1/2 m-4 z-50" src="/src/assets/kelly-sikkema-A3XL3jDki70-unsplash.jpeg" alt="Woman coding">
+        <div class="col-start-3 text-black text-2xl m-4 z-50">Hi Xync</div>
+      </div>
+    </div>
   </div>
 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'xl': '1.0rem',
+       }
+    },
   },
   variants: {
     extend: {},
