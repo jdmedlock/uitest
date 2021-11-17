9c3e1917a1b9564f770c7074522a23dbcd1b5dba
jdmedlock <jdmedlock@gmail.com>
2021-11-17 09:58:46 -0600

    Feature: Add additional positioning & layout refinements [WIP]

--- src/components/HighlightCard.svelte
+++ src/components/HighlightCard.svelte
@@ -1,21 +1,22 @@
-<div class="flex-none relative inset-0 mt-16 w-2/3 h-3/4 place-self-center">
+
+<div class="flex-none relative inset-0 w-2/3 h-1/3 place-self-center">
   <div class="transform rotate-2 drop-shadow-2xl ">
     <!-- Background card image -->
-    <img class="absolute rounded-xl z-0" src="/src/assets/green-card.png" alt="green card">
+    <img class="absolute rounded-xl z-0 scale-y-75" src="/src/assets/green-card.png" alt="green card">
 
     <!-- Content overlaying the card -->
-    <div class="flex-none grid-rows-3 gap-1">
+    <div class="flex-none grid-rows-3 gap-1 pt-32">
       <!-- Row #1-->
       <div class="flex w-full row-start-1 grid-cols-3 justify-evenly">
-        <div class="col-start-1 text-black text-2xl m-4 z-50">Hi Xync</div>
-        <div class="col-start-2 text-black text-2xl m-4 z-50">Hi Xync</div>
-        <div class="col-start-3 text-black text-2xl m-4 z-50">Hi Xync</div>
+        <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #1</div>
+        <div class="col-start-2 text-black text-2xl m-4 z-50">This is item #2</div>
+        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #3</div>
       </div>
       <!-- Row #2 -->
       <div class="flex w-full row-start-2 grid-cols-3 justify-evenly">
-        <div class="col-start-1 text-black text-2xl m-4 z-50">Hi Xync</div>
+        <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #4</div>
         <img class="col-start-2 w-1/2 m-4 z-50" src="/src/assets/kelly-sikkema-A3XL3jDki70-unsplash.jpeg" alt="Woman coding">
-        <div class="col-start-3 text-black text-2xl m-4 z-50">Hi Xync</div>
+        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #5</div>
       </div>
     </div>
   </div>
