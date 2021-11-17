038732b9d39ea83ba7a7150a87684ad5814ea114
jdmedlock <jdmedlock@gmail.com>
2021-11-17 12:56:37 -0600

    Feature: Right align text elements in the rightmost HighlightCard column

--- src/components/HighlightCard.svelte
+++ src/components/HighlightCard.svelte
@@ -8,21 +8,21 @@
     <div class="flex-none grid-rows-3 gap-4 pt-32">
       <!-- Row #1-->
       <div class="flex w-full row-start-1 grid-cols-3 justify-evenly place-items-center">
-        <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #1</div>
-        <div class="col-start-2 text-black text-2xl m-4 z-50">This is item #2</div>
-        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #3</div>
+        <div class="col-start-1 text-black text-3xl m-4 z-50">Level-up technical + soft skills</div>
+        <div class="col-start-2 text-black text-3xl m-4 z-50"></div>
+        <div class="col-start-3 text-black text-3xl text-right m-4 z-50">Learn with & from other Developers</div>
       </div>
       <!-- Row #2 -->
       <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center">
-        <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #4</div>
+        <div class="col-start-1 text-black text-3xl m-4 z-50"></div>
         <img class="col-start-2 w-1/2 m-4 z-50" src="/src/assets/kelly-sikkema-A3XL3jDki70-unsplash.jpeg" alt="Woman coding">
-        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #6</div>
+        <div class="col-start-3 text-black text-3xl text-right m-4 z-50"></div>
       </div>
       <!-- Row #3 -->
       <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center">
-        <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #7</div>
-        <div class="col-start-2 text-black text-2xl m-4 z-50">This is item #8</div>
-        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #9</div>
+        <div class="col-start-1 text-black text-3xl m-4 z-50"></div>
+        <div class="col-start-2 text-black text-3xl m-4 z-50">Build experience to get jobs!!!</div>
+        <div class="col-start-3 text-black text-3xl text-right m-4 z-50"></div>
       </div>
     </div>
   </div>
