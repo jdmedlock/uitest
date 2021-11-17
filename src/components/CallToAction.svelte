de0a951f0998f962f0ac623c04be1aa1c8648df4
jdmedlock <jdmedlock@gmail.com>
2021-11-17 13:55:31 -0600

    Feature: Add CallToAction component

---
+++ src/components/CallToAction.svelte
@@ -0,0 +1,8 @@
+<div class="flex place-self-center z-50 w-full m-4">
+  <div class="transform rotate-2">
+    <div class="flex text-black text-3xl">
+      <div class="w-2/5 ml-48">Are you ready to take your career to the next level?</div>
+      <button class="bg-green-700 rounded-xl text-white text-3xl w-1/5">Start Today!</button>
+    </div>
+  </div>
+</div>
--- src/containers/Highlight.svelte
+++ src/containers/Highlight.svelte
@@ -1,8 +1,10 @@
 <script>
+	import CallToAction from '../components/CallToAction.svelte'
 	import HighlightCard from '../components/HighlightCard.svelte'
 	import Quote from '../components/Quote.svelte'
 </script>
 
 <HighlightCard></HighlightCard>
 <Quote></Quote>
+<CallToAction></CallToAction>
 
