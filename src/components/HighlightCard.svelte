8870882f5e0d500fcd3fb4bbe652edc8d0eb27ea
jdmedlock <jdmedlock@gmail.com>
2021-11-17 10:11:33 -0600

    Feature: Align HighlightCard items horizontally in the center

--- src/components/HighlightCard.svelte
+++ src/components/HighlightCard.svelte
@@ -5,18 +5,24 @@
     <img class="absolute rounded-xl z-0 scale-y-75" src="/src/assets/green-card.png" alt="green card">
 
     <!-- Content overlaying the card -->
-    <div class="flex-none grid-rows-3 gap-1 pt-32">
+    <div class="flex-none grid-rows-3 gap-4 pt-32">
       <!-- Row #1-->
-      <div class="flex w-full row-start-1 grid-cols-3 justify-evenly">
+      <div class="flex w-full row-start-1 grid-cols-3 justify-evenly place-items-center">
         <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #1</div>
         <div class="col-start-2 text-black text-2xl m-4 z-50">This is item #2</div>
         <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #3</div>
       </div>
       <!-- Row #2 -->
-      <div class="flex w-full row-start-2 grid-cols-3 justify-evenly">
+      <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center">
         <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #4</div>
         <img class="col-start-2 w-1/2 m-4 z-50" src="/src/assets/kelly-sikkema-A3XL3jDki70-unsplash.jpeg" alt="Woman coding">
-        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #5</div>
+        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #6</div>
+      </div>
+      <!-- Row #3 -->
+      <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center">
+        <div class="col-start-1 text-black text-2xl m-4 z-50">This is item #7</div>
+        <div class="col-start-2 text-black text-2xl m-4 z-50">This is item #8</div>
+        <div class="col-start-3 text-black text-2xl m-4 z-50">This is item #9</div>
       </div>
     </div>
   </div>
