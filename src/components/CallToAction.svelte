2cf2ae4af0caaf29181e2cc47c1a4897ad6a9837
Jim Medlock <jdmedlock@gmail.com>
2021-11-18 20:07:19 -0600

    Feature: Add hover scaling & shadow to cta button

--- src/components/CallToAction.svelte
+++ src/components/CallToAction.svelte
@@ -1,6 +1,6 @@
 <div class="flex place-self-center w-full mt-8">
   <div class="flex text-black text-3xl">
     <div class="w-3/5">Are you ready to take your career to the next level?</div>
-    <button class="bg-green-700 rounded-xl text-white text-3xl w-1/3  ml-4 hover:scale-110">Start Today!</button>
+    <button class="bg-green-700 rounded-xl shadow-2xl text-white text-3xl w-1/3  ml-4 transform transition duration-500 hover:scale-110">Start Today!</button>
   </div>
 </div>
