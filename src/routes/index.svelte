9b89cacc1e8bb997787a2b94a1003597dfef92d4
jdmedlock <jdmedlock@gmail.com>
2021-11-19 16:56:52 -0600

    Feature: Cleanup header & footer

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,53 +10,40 @@
 	<title>Home</title>
 </svelte:head>
 
-<section>
-	<h1>
+<section class="flex-1 flex-col justify-center text-center">
+	<h1 class="text-bold text-2xl">
 		Welcome to my UI prototype
 	</h1>
-	<p>
+	<p class="mt-8 text-2xl">
 		This is where I'm testing some ideas for a new Chingu.io website.
 	</p>
 
-	<h2 class="mb-12">This is where the magic happens</h2>
-	<Highlight 
-		config={{
-			rotate: 'left',
-			cardConfig: {
-				tagLines: {
-					line1: "Transform what you learn...",
-					line2: "into Experience to get Jobs!!!",
+	<p class="mb-12 text-2xl">This is where the magic happens</p>
+	<div class="flex justify-center">
+		<Highlight
+			config={{
+				rotate: 'left',
+				cardConfig: {
+					tagLines: {
+						line1: "Transform what you learn...",
+						line2: "into Experience to get Jobs!!!",
+					},
+					textLines: { 
+						line1: 'Level-up technical + soft skills',
+						line2: '',
+						line3: 'Learn with & from other Developers',
+						line4: '',		
+						line5: '',
+						line6: '',
+						line7: 'Build experience to get jobs!!!',
+						line8: '',
+					}, 
+					imageURL: '/assets/kelly-sikkema-A3XL3jDki70-unsplash.jpeg',
 				},
-				textLines: { 
-					line1: 'Level-up technical + soft skills',
-					line2: '',
-					line3: 'Learn with & from other Developers',
-					line4: '',		
-					line5: '',
-					line6: '',
-					line7: 'Build experience to get jobs!!!',
-					line8: '',
-				}, 
-				imageURL: '/assets/kelly-sikkema-A3XL3jDki70-unsplash.jpeg',
-			},
-			quoteConfig: {
-				quote: '"This is one awesome quote"...Alfred E. Newman'
-			},
-		}}>
-	</Highlight>
+				quoteConfig: {
+					quote: '"This is one awesome quote"...Alfred E. Newman'
+				},
+			}}>
+		</Highlight>
+	</div>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-</style>
