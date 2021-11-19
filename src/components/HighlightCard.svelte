0efe629c5ba8900d18f17051c5555cca9df30d37
jdmedlock <jdmedlock@gmail.com>
2021-11-19 11:26:09 -0600

    Feature: Refine responsive layout

--- src/components/HighlightCard.svelte
+++ src/components/HighlightCard.svelte
@@ -37,26 +37,26 @@
       <!-- Content overlaying the card -->
       <div class="flex-none grid-rows-3 gap-4">
         <!-- Row #1-->
-        <div class="flex w-full row-start-1 grid-cols-3 justify-evenly place-items-center text-center">
-          <div class="col-start-1 text-black text-3xl m-4">{text.line1}</div>
-          <div class="col-start-2 text-black text-3xl m-4">{text.line2}</div>
-          <div class="col-start-3 text-black text-3xl m-4">{text.line3}</div>
+        <div class="flex w-full row-start-1 grid-cols-3 justify-evenly place-items-center text-center text-black text-2xl md:text-3xl">
+          <div class="col-start-1 m-4">{text.line1}</div>
+          <div class="col-start-2 m-4">{text.line2}</div>
+          <div class="col-start-3 m-4">{text.line3}</div>
         </div>
         <!-- Row #2 -->
-        <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center text-center">
-          <div class="col-start-1 text-black text-3xl m-4">{text.line4}</div>
-          <img class="col-start-2 w-1/2 m-4 z-50 rounded-xl shadow-2xl" src="{imageURL}" alt="Woman coding">
-          <div class="col-start-3 text-black text-3xl m-4">{text.line5}</div>
+        <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center text-center text-2xl md:text-3xl">
+          <div class="col-start-1 text-black m-4">{text.line4}</div>
+          <img class="col-start-2 transform scale-125 md:scale-100 w-1/2 m-4 z-50 rounded-xl shadow-2xl" src="{imageURL}" alt="Woman coding">
+          <div class="col-start-3 text-black m-4">{text.line5}</div>
         </div>
         <!-- Row #3 -->
-        <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center text-center">
-          <div class="col-start-1 text-black text-3xl m-4">{text.line6}</div>
-          <div class="col-start-2 text-black text-3xl m-4">{text.line7}</div>
-          <div class="col-start-3 text-black text-3xl m-4">{text.line8}</div>
+        <div class="flex w-full row-start-2 grid-cols-3 justify-evenly place-items-center text-center text-2xl md:text-3xl">
+          <div class="col-start-1 text-black m-4">{text.line6}</div>
+          <div class="col-start-2 text-black m-4">{text.line7}</div>
+          <div class="col-start-3 text-black m-4">{text.line8}</div>
         </div>
       </div>
     
-      <div class="bg-green-200 rounded-xl pt-0 pb-8 pl-8 ">
+      <div class="bg-green-200 rounded-xl pt-0 pb-8">
         <Quote {quote}></Quote>
         <CallToAction></CallToAction>
       </div>
--- src/components/Quote.svelte
+++ src/components/Quote.svelte
@@ -2,9 +2,9 @@
   export let quote = ''
 </script>
 
-<div class="w-max place-self-center w-full m-4 -ml-20">
+<div class=" w-screen place-self-center m-2 md:m-4 -ml-4 md:-ml-20">
   <div class="flex content-center">
-    <div class="bg-green-500 text-white text-4xl border-solid border-2 border-gray-400">
+    <div class="bg-green-500 text-white text-2xl md:text-4xl border-solid border-2 border-gray-400">
       <div class="m-4">{quote}</div>
     </div>
   </div>
