d27621a85dfd6f55c809c91b3c9aaf2dd168a737
jdmedlock <jdmedlock@gmail.com>
2021-11-19 11:31:29 -0600

    Feature: Fix responsive alignment of quote on tablets

--- src/components/Quote.svelte
+++ src/components/Quote.svelte
@@ -2,7 +2,7 @@
   export let quote = ''
 </script>
 
-<div class=" w-screen place-self-center m-2 md:m-4 -ml-4 md:-ml-20">
+<div class=" w-screen place-self-center m-2 lg:m-4 -ml-4 lg:-ml-20">
   <div class="flex content-center">
     <div class="bg-green-500 text-white text-2xl md:text-4xl border-solid border-2 border-gray-400">
       <div class="m-4">{quote}</div>
