e8c31b831545962105a92df1b43d5ba0d0827030
jdmedlock <jdmedlock@gmail.com>
2021-11-21 08:02:28 -0600

    Refactor: Correct <img> alt attribute to remove warning message

--- src/components/ContentCell.svelte
+++ src/components/ContentCell.svelte
@@ -8,7 +8,7 @@
     <div {style}>{contentCell.value ? contentCell.value : ''}</div>
   {:else}
     {#if contentCell.type === 'img'}
-      <img class="{style} transform scale-125 md:scale-100 w-1/2 m-4 z-50 rounded-xl shadow-2xl" src="{contentCell.value}" alt="cell image">
+      <img class="{style} transform scale-125 md:scale-100 w-1/2 m-4 z-50 rounded-xl shadow-2xl" src="{contentCell.value}" alt="cell">
     {/if}
   {/if}
 {/if}
